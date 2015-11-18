e9299d95987de2e1fbfb3c0ab9ec2e3afd984537
Michael DeMarco <michael.demarco65@gmail.com>
2015-11-17 22:04:36 -0700

    greeting
    saying Hey and saying Yo on the terminal

---
+++ greeting.rb
@@ -0,0 +1,24 @@
+def say_hey
+    
+    
+    ARGV.each do |arg|
+        puts "Hey " + "#{arg}"
+        
+    end
+    end
+
+say_hey
+
+def say_yo
+    
+    
+    ARGV.each do |arg|
+        puts "Yo " + "#{arg}"
+        
+    end
+end
+
+say_yo
+
+
+
