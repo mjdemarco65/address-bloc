9abf389c02179aeeb7fec00697530a4ca4ae81d3
Michael DeMarco <michael.demarco65@gmail.com>
2015-12-01 16:50:34 -0700

    address book main file
    Bloc exercise 19

--- models/address_book.rb
+++ models/address_book.rb
@@ -3,11 +3,23 @@ require_relative 'entry'
 
 class AddressBook
     attr_accessor :entries
-    
+
     def initialize
         @entries = []
     end
-    
+
+    def remove_entry(name, phone_number, email)
+        delete_entry = nil
+
+        @entries.each do |entry|
+          if name == entry.name && phone_number == entry.phone_number && email == entry.email
+            delete_entry = entry
+          end
+        end
+
+      @entries.delete(delete_entry)
+    end
+
     def add_entry(name, phone_number, email)
         # #9
         index = 0
